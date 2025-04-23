715f2f71a7e4fba20606547153f63b58e65ec186
MostafaJammoul <mwj06@mail.aub.edu>
2025-04-24 01:59:13 +0300

    prometheus changes, fixed

--- image_model/app.py
+++ image_model/app.py
@@ -11,12 +11,12 @@ from fastapi.responses import Response
 
 # ╭──────────────── Config ───────────────╮
 load_dotenv()
-AZ_CONN     = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+AZ_CONN     = "DefaultEndpointsProtocol=https;AccountName=modelscsv;AccountKey=hC/W3DHx3nxkNHhbJCiOBRICi56Cy/htx2lWQoI6LRO8hT5mKWVKIoIlEmHte6oLE6003sBGTalp+AStL5lznw==;EndpointSuffix=core.windows.net"
 CONTAINER   = "fakenewsdetection-models"
 PROD_BLOB   = "image.pt"
 SHAD_BLOB   = "shadow_image.pt"
 #LOCAL_WEIGHTS_DIR = os.getenv("WEIGHTS_DIR", os.path.dirname(__file__))
-ADMIN_TOKEN = os.getenv("ADMIN_TOKEN")
+ADMIN_TOKEN = "admin@123"
 device      = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # ╰────────────────────────────────────────╯
 
